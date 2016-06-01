5397fd6ba03f5dbad9494878bf7bdba101ff252c
TomasDuro <duro.tomas@gmail.com>
2016-06-01 12:57:54 +0200

    KCF Visual Tracker
    Implementation of the Kernelized Correlation Filter tracker.
    
    http://www.robots.ox.ac.uk/~joao/circulant/

---
+++ sw/airborne/modules/computer_vision/trackers/KCF.c
@@ -0,0 +1,53 @@
+/*
+* Copyright (C) Tomás Duro
+*
+* This file is part of paparazzi
+*
+* paparazzi is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* paparazzi is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with paparazzi; see the file COPYING.  If not, see
+* <http://www.gnu.org/licenses/>.
+*/
+/**
+* @file "modules/computer_vision/trackers/KCF.c"
+* @author Tomás Duro
+* KCF tracker implementation.http://www.robots.ox.ac.uk/~joao/circulant/
+*/
+
+#include "modules/computer_vision/trackers/KCF.h"
+#include "modules/computer_vision/cv.h"
+#include "subsystems/datalink/telemetry.h"
+#include <stdio.h>
+// #include "subsystems/datalink/telemetry.h"
+// #include <std.h>
+
+// BOUNDING BOX RESULT
+extern int Xa;
+extern int Ya;
+extern int Wa;
+extern int Ha;
+
+extern int initKey;
+
+// KCF TRACKER FUNCTION
+int KCF(struct image_t* img);
+
+bool_t KCF_func(struct image_t* unimg)
+{
+  KCF(imgCrop);
+  return FALSE;
+}
+
+void KCF_init(void)
+{
+  cv_add(KCF_func);
+}
---
+++ sw/airborne/modules/computer_vision/trackers/KCFmain.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+#include <opencv2/core/core.hpp>
+#include <dirent.h>
+#include "modules/computer_vision/lib/vision/image.h"
+#include "kcftracker.hpp"
+
+using namespace std;
+using namespace cv;
+
+//INITIAL BOUNDING BOX
+int Xa = 50;
+int Ya = 20;
+int Wa = 20;
+int Ha = 40;
+
+int initKey = 0;
+
+extern "C" {
+	int KCF(struct image_t* img);
+}
+
+int KCF(struct image_t* img)
+{
+	static int isFirstTime = 1;
+
+	bool HOG = true;
+	bool FIXEDWINDOW = false;
+	bool MULTISCALE = true;
+	bool LAB = false;
+
+	static KCFTracker tracker(HOG, FIXEDWINDOW, MULTISCALE, LAB);
+	static Rect result;
+
+	//CONVERT IMAGE FROM YUV422 TO BGR
+	Mat image(img->h,img->w,CV_8UC3);
+
+	uchar *bgrbuf = image.data;
+	uint8_t *imgbuf = (uint8_t *) img->buf;
+
+	for (uint16_t y = 0; y < img->h; y++) {
+		for (uint16_t x = 0; x < img->w; x += 2) {
+			int aux = 1.164*(imgbuf[1]-16)+2.018*(imgbuf[0]-128);
+			aux = (aux > 255) ? 255 : aux;
+			aux = (aux < 0) ? 0 : aux;
+			bgrbuf[0] = aux;
+			aux = 1.164*(imgbuf[1]-16)-0.813*(imgbuf[2]-128)-0.391*(imgbuf[0]-128);
+			aux = (aux > 255) ? 255 : aux;
+			aux = (aux < 0) ? 0 : aux;
+			bgrbuf[1] = aux;
+			bgrbuf[2] = 1.164*(imgbuf[1]-16)+1.596*(imgbuf[2]-128);
+			aux = (aux > 255) ? 255 : aux;
+			aux = (aux < 0) ? 0 : aux;
+			bgrbuf[2] = aux;
+			bgrbuf[3] = 1.164*(imgbuf[3]-16)+2.018*(imgbuf[0]-128);
+			aux = (aux > 255) ? 255 : aux;
+			aux = (aux < 0) ? 0 : aux;
+			bgrbuf[3] = aux;
+			bgrbuf[4] = 1.164*(imgbuf[3]-16)-0.813*(imgbuf[2]-128)-0.391*(imgbuf[0]-128);
+			aux = (aux > 255) ? 255 : aux;
+			aux = (aux < 0) ? 0 : aux;
+			bgrbuf[4] = aux;
+			bgrbuf[5] = 1.164*(imgbuf[3]-16)+1.596*(imgbuf[2]-128);
+			aux = (aux > 255) ? 255 : aux;
+			aux = (aux < 0) ? 0 : aux;
+			bgrbuf[5] = aux;
+
+			imgbuf += 4;
+			bgrbuf += 6;
+		}
+	}
+
+	//TRACKER MAIN LOOP
+	if (initKey > 30)
+	{
+		if (isFirstTime)
+		{
+			tracker.init(Rect(Xa,Ya,Wa,Ha),image);
+			isFirstTime = 0;
+		}
+		else
+		{
+			result = tracker.update(image);
+
+			Xa = result.x;
+			Ya = result.y;
+			Wa = result.width;
+			Ha = result.height;
+		}
+	}
+	else
+	{
+		initKey++;
+	}
+
+	return 1;
+}
